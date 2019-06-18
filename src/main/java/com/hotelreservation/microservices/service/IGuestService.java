17ef88bbf1cf6c928570cac11680d6158bf961a5
e068635 <elson.franca@mastercard.com>
2019-06-18 16:31:57 -0300

    initial version

---
+++ src/main/java/com/hotelreservation/microservices/service/IGuestService.java
@@ -0,0 +1,53 @@
+package com.hotelreservation.microservices.service;
+
+import com.hotelreservation.microservices.entity.Guest;
+import com.hotelreservation.microservices.exceptions.GuestAlreadyRegisteredException;
+import com.hotelreservation.microservices.exceptions.GuestNotFoundException;
+import com.hotelreservation.microservices.vo.GuestVO;
+
+import java.util.List;
+
+/**
+ * Created by e068635 on 6/10/2019.
+ */
+public interface IGuestService {
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    GuestVO findGuestById(Long id)  throws GuestNotFoundException;
+
+    /**
+     *
+     * @param lastName
+     * @return
+     */
+    GuestVO findGuestByLastName(String lastName)  throws GuestNotFoundException;
+
+
+    /**
+     *
+     * @param newGuest
+     * @return
+     * @throws GuestAlreadyRegisteredException
+     */
+    GuestVO registerNewGuest(Guest newGuest) throws GuestAlreadyRegisteredException;
+
+    /**
+     *
+     * @param guestId
+     * @return
+     * @throws GuestNotFoundException
+     */
+    void deleteGuest(Long guestId) throws GuestNotFoundException;
+
+    /**
+     *
+     * @return
+     */
+    List<GuestVO> findAll();
+
+
+}
